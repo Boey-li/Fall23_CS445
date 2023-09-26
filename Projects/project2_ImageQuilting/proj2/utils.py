18260dc932594add2b80ba866d8a40eb2c96b972
Boey-li <baoyul2@illinois.edu>
2023-09-26 16:46:47 -0500

    Done project 2.

--- Projects/project2_ImageQuilting/proj2/utils.py
+++ Projects/project2_ImageQuilting/proj2/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 
 def cut(err_patch):
     """
@@ -40,3 +41,20 @@ def cut(err_patch):
         mask[:best_path[x-1], x-1] = 1
     mask ^= 1
     return mask
+
+def gaussian_kernel(sigma, kernel_half_size):
+    '''
+    Copy from Project1 Hybrid Images
+    Inputs:
+        sigma = standard deviation for the gaussian kernel
+        kernel_half_size = recommended to be at least 3*sigma
+    
+    Output:
+        Returns a 2D Gaussian kernel matrix
+    '''
+    window_size = kernel_half_size*2+1
+    gaussian_kernel_1d = signal.gaussian(window_size, std=sigma).reshape(window_size, 1)
+    gaussian_kernel_2d = np.outer(gaussian_kernel_1d, gaussian_kernel_1d)
+    gaussian_kernel_2d /= np.sum(gaussian_kernel_2d) # make sure it sums to one
+
+    return gaussian_kernel_2d
